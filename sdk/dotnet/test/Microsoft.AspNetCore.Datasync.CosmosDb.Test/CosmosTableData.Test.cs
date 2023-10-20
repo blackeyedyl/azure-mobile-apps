144e18321bd5949664164acd48ed73f547ec8328
Dylan Mills <dylan.mills@irisndt.com>
2023-10-20 08:22:50 -0600

    Add cosmos object test for compound id

--- sdk/dotnet/test/Microsoft.AspNetCore.Datasync.CosmosDb.Test/CosmosTableData.Test.cs
+++ sdk/dotnet/test/Microsoft.AspNetCore.Datasync.CosmosDb.Test/CosmosTableData.Test.cs
@@ -28,7 +28,7 @@ public class CosmosTableData_Tests
     public void Deleted_CanBeRead(bool deleted)
     {
         // Arrange
-        var source = new Entity { Id = "test", EntityTag = null, UpdatedAt = DateTimeOffset.Now, Deleted = deleted };
+        var source = new CosmosTestEntity { Id = "test", EntityTag = null, UpdatedAt = DateTimeOffset.Now, Deleted = deleted };
 
         // Assert
         Assert.Equal(deleted, source.Deleted);
@@ -38,7 +38,7 @@ public class CosmosTableData_Tests
     public void EntityTag_IsSet_WhenVersionUpdated()
     {
         // Arrange
-        var source = new Entity { Id = "test", Version = Encoding.UTF8.GetBytes("abcd"), UpdatedAt = DateTimeOffset.Now };
+        var source = new CosmosTestEntity { Id = "test", Version = Encoding.UTF8.GetBytes("abcd"), UpdatedAt = DateTimeOffset.Now };
 
         // Assert
         Assert.Equal("abcd", source.EntityTag);
@@ -56,8 +56,8 @@ public class CosmosTableData_Tests
         [CombinatorialRange(0, 3)] int bVersionIndex)
     {
         // Arrange
-        var a = new Entity { Id = aID, Deleted = aDeleted, UpdatedAt = testTimes[aUpdatedIndex], Version = testVersions[aVersionIndex] };
-        var b = new Entity { Id = bID, Deleted = bDeleted, UpdatedAt = testTimes[bUpdatedIndex], Version = testVersions[bVersionIndex] };
+        var a = new CosmosTestEntity { Id = aID, Deleted = aDeleted, UpdatedAt = testTimes[aUpdatedIndex], Version = testVersions[aVersionIndex] };
+        var b = new CosmosTestEntity { Id = bID, Deleted = bDeleted, UpdatedAt = testTimes[bUpdatedIndex], Version = testVersions[bVersionIndex] };
         var expected = aID == bID && aDeleted == bDeleted && aUpdatedIndex == bUpdatedIndex && aVersionIndex == bVersionIndex;
 
         // Act
@@ -67,11 +67,25 @@ public class CosmosTableData_Tests
         Assert.Equal(expected, actual);
     }
 
+    [Theory]
+    [InlineData("id", "Short")]
+    [InlineData("id-0000", "Empty")]
+    [InlineData("id-000 is super long", "Long")]
+    [InlineData("id-300", "Test")]
+    public void Equals_CompoundId(string id, string partitionKey)
+    {
+        // Arrange
+        var entity = new { Id = id, PartitionKey = partitionKey };
+
+        // Act & Assert
+        Assert.Equal($"{id}:{partitionKey}", entity.Id);
+    }
+
     [Fact]
     public void Equals_NullOther_ReturnsFalse()
     {
         // Arrange
-        var source = new Entity { Id = "test", Version = testVersions[1], UpdatedAt = DateTimeOffset.Now };
+        var source = new CosmosTestEntity { Id = "test", Version = testVersions[1], UpdatedAt = DateTimeOffset.Now };
         ITableData other = null;
 
         // Act
@@ -85,16 +99,16 @@ public class CosmosTableData_Tests
     public void Equals_Works([CombinatorialRange(0, 5)] int offset)
     {
         DateTimeOffset dto = DateTimeOffset.Parse("2021-01-01T01:00:00Z");
-        List<Entity> testEntities = new()
+        List<CosmosTestEntity> testEntities = new()
         {
             null,
-            new Entity { Id = "nottest", EntityTag = "abcd", UpdatedAt = dto, Deleted = false },
-            new Entity { Id = "test", EntityTag = "efgh", UpdatedAt = dto, Deleted = false },
-            new Entity { Id = "test", EntityTag = "abcd", UpdatedAt = DateTimeOffset.UtcNow, Deleted = false },
-            new Entity { Id = "test", EntityTag = "abcd", UpdatedAt = dto, Deleted = true }
+            new CosmosTestEntity { Id = "nottest", EntityTag = "abcd", UpdatedAt = dto, Deleted = false },
+            new CosmosTestEntity { Id = "test", EntityTag = "efgh", UpdatedAt = dto, Deleted = false },
+            new CosmosTestEntity { Id = "test", EntityTag = "abcd", UpdatedAt = DateTimeOffset.UtcNow, Deleted = false },
+            new CosmosTestEntity { Id = "test", EntityTag = "abcd", UpdatedAt = dto, Deleted = true }
         };
         var test = testEntities[offset];
-        var source = new Entity { Id = "test", EntityTag = "abcd", UpdatedAt = DateTimeOffset.Parse("2021-01-01T01:00:00Z"), Deleted = false };
+        var source = new CosmosTestEntity { Id = "test", EntityTag = "abcd", UpdatedAt = DateTimeOffset.Parse("2021-01-01T01:00:00Z"), Deleted = false };
 
         Assert.False(source.Equals(test));
     }
@@ -103,7 +117,7 @@ public class CosmosTableData_Tests
     public void Version_Empty_WhenEntityTagEmpty()
     {
         // Arrange
-        var source = new Entity { Id = "test", EntityTag = null, UpdatedAt = DateTimeOffset.Now };
+        var source = new CosmosTestEntity { Id = "test", EntityTag = null, UpdatedAt = DateTimeOffset.Now };
 
         // Assert
         Assert.Empty(source.Version);
@@ -113,13 +127,14 @@ public class CosmosTableData_Tests
     public void Version_IsSet_WhenEntityTagUpdated()
     {
         // Arrange
-        var source = new Entity { Id = "test", EntityTag = "abcd", UpdatedAt = DateTimeOffset.Now };
+        var source = new CosmosTestEntity { Id = "test", EntityTag = "abcd", UpdatedAt = DateTimeOffset.Now };
 
         // Assert
         Assert.Equal(new byte[] { 0x61, 0x62, 0x63, 0x64 }, source.Version);
     }
 
-    private class Entity : CosmosTableData
+    private class CosmosTestEntity : CosmosTableData
     {
+        public string PartitionKey { get; set; }
     }
 }
